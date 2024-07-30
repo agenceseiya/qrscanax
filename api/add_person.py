e25daf0aa0cace37c7a1fdd7af8e779731fb1ca5
agenceseiya <166331355+agenceseiya@users.noreply.github.com>
2024-07-30 15:08:22 +0300

    Update add_person.py

--- api/add_person.py
+++ api/add_person.py
@@ -1,12 +1,19 @@
 from http.server import BaseHTTPRequestHandler
 import json
+import os
 import gspread
 from google.oauth2.service_account import Credentials
 
+def get_credentials():
+    creds_json = os.environ.get('GOOGLE_APPLICATION_CREDENTIALS')
+    creds_dict = json.loads(creds_json)
+    return Credentials.from_service_account_info(creds_dict)
+
 def add_person_to_sheet(firstname, lastname, email, telephone):
-    scope = ['https://spreadsheets.google.com/feeds', 'https://www.googleapis.com/auth/drive']
-    creds = Credentials.from_service_account_file('path/to/credentials.json', scopes=scope)
+    creds = get_credentials()
     client = gspread.authorize(creds)
+    
+    # Make sure to replace 'Visitor Database' with your actual Google Sheet name
     sheet = client.open('Visitor Database').sheet1
     
     row = [firstname, lastname, email, telephone]
@@ -19,10 +26,15 @@ class handler(BaseHTTPRequestHandler):
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data.decode('utf-8'))
 
-        qr_data = add_person_to_sheet(data['firstname'], data['lastname'], data['email'], data['telephone'])
+        try:
+            qr_data = add_person_to_sheet(data['firstname'], data['lastname'], data['email'], data['telephone'])
+            response = json.dumps({'success': True, 'qr_data': qr_data})
+            status_code = 200
+        except Exception as e:
+            response = json.dumps({'success': False, 'error': str(e)})
+            status_code = 500
 
-        self.send_response(200)
+        self.send_response(status_code)
         self.send_header('Content-type', 'application/json')
         self.end_headers()
-        response = json.dumps({'success': True, 'qr_data': qr_data})
         self.wfile.write(response.encode('utf-8'))
